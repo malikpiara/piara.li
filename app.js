4b1ed59756f40f5b587e075e37b1bda8fea9e3ad
Malik Piara <malik@hey.com>
2022-12-05 09:31:17 +0000

    Updated book url and removed legacy code.
    Legacy was moved to Cobra.

--- app.js
+++ app.js
@@ -30,24 +30,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(allowCrossDomain);
 app.use('/', indexRouter);
 
-// Temporary comments data structure. To be moved to another API soon.
-// TODO: Create a parameter that enables people to ask for the post_id.
-const mwComments = 
-  [
-    {
-      'id': 'c00001',
-      'author': 'John Green',
-      'content': 'Always on point, needed a jolt here as I feel in between two of these jobs, just need to embrace where I am and keep learning and growing.',
-      'post_id': 'copy'
-    },
-    {
-      'id': 'c00002',
-      'author': 'Hank Green',
-      'content': 'Always on point, needed a bolt here as I feel in between two of these jobs, just need to embrace where I am and keep learning and thriving.',
-      'post_id': 'copy'
-    }
-  ];
-
 const urlKeys = new Map([
   ['upf', 'upframe.io'],
   ['mw','moonwith.com'],
@@ -56,7 +38,7 @@ const urlKeys = new Map([
   ['l', 'linkedin.com/in/malikpiara'],
   ['ig', 'instagram.com/malikpiara'],
   ['earnest', 'play.google.com/store/apps/details?id=com.glenncoding.earnestcards'],
-  ['book', 'moonwith.com/book'],
+  ['book', 'calendar.app.google/ncTkKc7MsvyEqTJq8'],
   ['berlin', 't.me/+OGAZMUob59w1OTJk'],
   ['joni', 'jonathanfreiberger.de/'],
   ['camelstripe', 'form.typeform.com/to/Z2hgSqGJ'],
@@ -70,34 +52,9 @@ let urlKeysToJson = JSON.stringify(Object.fromEntries(urlKeys));
 // Accessing a key.
 // let json = JSON.parse(urlKeysToJson);
 
-// Posting comments
-app.post("/post/:post_id/:author/:content", function(req, res) {
-  mwComments.push(
-    {
-      'id': 'c00003',
-      'author': req.params.author,
-      'content': req.params.content,
-      'post_id': req.params.post_id
-    }
-  )
-  res.send(mwComments)
-})
-
 // JSON endpoint
 app.get('/api', function(req, res) {
   res.send(urlKeysToJson);
-});
-
-// Endpoint for Comments. TODO: Move to another API.
-
-app.get('/comments', function(req, res) {
-  const commentsToJSON = JSON.stringify(mwComments);
-  res.send(commentsToJSON);
-});
-
-app.get('/comments/:post_id', function(req, res) {
-  const commentsToJSON = JSON.stringify(mwComments.filter(comment => comment.post_id == req.params.post_id));
-  res.send(commentsToJSON);
 });
 
 app.get('/:id', function(req, res) {
